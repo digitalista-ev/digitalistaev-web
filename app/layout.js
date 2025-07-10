c3c9c3c955f148310ef5f8fd13bff7df9031257b
vakhnenzon <vakhnenzon@mail.de>
2025-07-10 19:42:18 +0200

    Fix og-image

--- app/layout.js
+++ app/layout.js
@@ -8,24 +8,26 @@ const imageUrl = `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/og-image.jpg`;
 
 export const metadata = {
   metadataBase: new URL("https://digitalista-ev.de"),
-  title: "Digitalista – Machinery help for humans",
+  title: "Digitalista - Machinery help for humans",
   description: "Digitalista - Unsere Projekte und Dokumente",
   keywords: ["Digitalista", "Ehrenamt", "Digitalisierung", "Projekte", "Spenden", "Verein"],
   authors: [{ name: "Digitalista e.V." }],
   openGraph: {
-    title: "Digitalista – Machinery help for humans",
+    title: "Digitalista - Machinery help for humans",
     description: "Digitalista - Unsere Projekte und Dokumente",
     type: "website",
     url: "https://digitalista-ev.de",
     images: [
       {
         url: imageUrl,
+        "width": 1200,
+        "height": 630,
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Digitalista – Machinery help for humans",
+    title: "Digitalista - Machinery help for humans",
     description: "Digitalista - Unsere Projekte und Dokumente",
     images: [imageUrl],
   },
