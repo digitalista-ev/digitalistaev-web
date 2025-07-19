330d137b450a7c07b2d7cdd22bb91cba4b334591
Aleksey Vakhnenko <gmail@vakhnenko.com>
2025-07-19 09:46:09 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig = {
     basePath: isDev ? '' : '', // '/digitalistaev-web',
-    assetPrefix: isDev ? '' : '', // '/digitalistaev-web/',
+    assetPrefix: isDev ? '' : '/', // '/digitalistaev-web/',
     images: {
         unoptimized: true,
     },
     env: {
-        NEXT_PUBLIC_BASE_PATH: isDev ? '' : '', // '/digitalistaev-web', // src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/next.svg`}
+        NEXT_PUBLIC_BASE_PATH: isDev ? '' : '', // '/digitalistaev-web',
     },
 };
 
